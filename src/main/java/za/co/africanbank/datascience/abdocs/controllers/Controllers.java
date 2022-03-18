094da5d969f1edeaa9337e332a96adae5f830b46
Collen Mabotja <cmabotja@africanbank.co.za>
2022-03-18 11:15:30 +0000

    Update Controllers.java

--- src/main/java/za/co/africanbank/datascience/abdocs/controllers/Controllers.java
+++ src/main/java/za/co/africanbank/datascience/abdocs/controllers/Controllers.java
@@ -277,8 +277,7 @@ public class Controllers {
 		//redirAttrs.addFlashAttribute("Email",mai.getFromAddress());
 		model.addAttribute("Role",user.getUserDetail(request.getRemoteUser()).getRole());
 		model.addAttribute("FullNames",user.getUserDetail(request.getRemoteUser()).getFullNames());
-		//redirAttrs.addFlashAttribute("path",path.replace("P:", "\\\\Neptune\\directdoc$")); 
-		redirAttrs.addFlashAttribute("path","/tmp/files");
+		redirAttrs.addFlashAttribute("path",path.replace("P:", "\\\\Neptune\\directdoc$"));
 		return "redirect:/WrapUp";
 
 
